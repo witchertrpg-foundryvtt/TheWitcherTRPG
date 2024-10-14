8ebab1ff4080d0c9e4ea3be3509875e03dc447ba
moliloo <dev.murilobrito@gmail.com>
2024-10-13 23:50:34 -0300

    adding defining skill and skill notes to monster sheet
    resolve changes request

--- module/TheWitcherTRPG.js
+++ module/TheWitcherTRPG.js
@@ -50,6 +50,8 @@ async function preloadHandlebarsTemplates() {
         'systems/TheWitcherTRPG/templates/partials/monster/monster-spell-tab.hbs',
         'systems/TheWitcherTRPG/templates/partials/monster/monster-skill-display.hbs',
         'systems/TheWitcherTRPG/templates/partials/monster/monster-custom-skill-display.hbs',
+        'systems/TheWitcherTRPG/templates/partials/monster/monster-tab-skills.hbs',
+        'systems/TheWitcherTRPG/templates/partials/monster/monster-profession-skill-display.hbs',
 
         'systems/TheWitcherTRPG/templates/partials/loot/loot-item-display.hbs',
 
@@ -350,3 +352,4 @@ Handlebars.registerHelper('formatModLabel', function (statCurrent, statMax) {
     let calc = statCurrent - statMax;
     return calc;
 });
+
