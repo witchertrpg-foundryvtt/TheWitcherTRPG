ccf2652cc0f6acd4f79f066d7b131dc8e41a01bf
Stexinator <hurricane90@gmx.de>
2024-12-18 08:33:28 +0100

    add weapon enhancements to weapon section

--- module/actor/sheets/WitcherActorSheet.js
+++ module/actor/sheets/WitcherActorSheet.js
@@ -137,9 +137,7 @@ export default class WitcherActorSheet extends ActorSheet {
     _prepareItems(context) {
         let items = context.items;
 
-        context.enhancements = items.filter(
-            i => i.type == 'enhancement' && i.system.type != 'armor' && !i.system.applied
-        );
+        context.enhancements = items.filter(i => i.type == 'enhancement' && !i.system.applied);
         context.runeItems = context.enhancements.filter(e => e.system.type == 'rune');
         context.glyphItems = context.enhancements.filter(e => e.system.type == 'glyph');
         context.containers = items.filter(i => i.type == 'container');
@@ -149,7 +147,13 @@ export default class WitcherActorSheet extends ActorSheet {
     }
 
     _prepareWeapons(context) {
-        context.weapons = context.actor.getList('weapon');
+        context.weapons = context.items.filter(function (item) {
+            return (
+                item.type == 'weapon' ||
+                (item.type == 'enhancement' && item.system.type == 'weapon' && item.system.applied == false)
+            );
+        });
+
         context.weapons.forEach(weapon => {
             if (
                 weapon.system.enhancements > 0 &&
