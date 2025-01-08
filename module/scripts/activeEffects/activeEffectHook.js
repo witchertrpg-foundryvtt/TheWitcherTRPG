765521057e19b373e2b1f7750314c53c4d3656d9
Stexinator <hurricane90@gmx.de>
2025-01-08 19:49:59 +0100

    automatically remove expired item effect

--- module/scripts/activeEffects/activeEffectHook.js
+++ module/scripts/activeEffects/activeEffectHook.js
@@ -2,8 +2,6 @@ export async function removeExpiredActiveEffects(combat) {
     if (!game.user.isGM) return;
 
     combat.combatants.forEach(combatant => {
-        let expiredEffects = combatant.actor.effects.filter(effect => effect.duration.remaining === 0);
-
-        expiredEffects.forEach(effect => effect.delete());
+        combatant.actor.handleExpiredEffects();
     });
 }
