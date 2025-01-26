621945bd5bbe2e2479e3fb25c0e3144b0e388ba8
Stexinator <hurricane90@gmx.de>
2025-01-26 11:52:31 +0100

    move monster custom stats to config

--- module/data/actor/templates/common/stats/derivedStatData.js
+++ module/data/actor/templates/common/stats/derivedStatData.js
@@ -1,14 +1,14 @@
-import modifier from "../modifierData.js";
+import modifier from '../modifierData.js';
 
 const fields = foundry.data.fields;
 
 export default function derivedStat(label) {
     return {
-        max: new fields.NumberField({ initial: 0}),
-        unmodifiedMax: new fields.NumberField({ initial: 0}),
-        value: new fields.NumberField({ initial: 0}),
-        label: new fields.StringField({ initial: label}),
+        max: new fields.NumberField({ initial: 0 }),
+        unmodifiedMax: new fields.NumberField({ initial: 0, label }),
+        value: new fields.NumberField({ initial: 0 }),
+        label: new fields.StringField({ initial: label }),
         modifiers: new fields.ArrayField(new fields.SchemaField(modifier())),
-        totalModifiers: new fields.NumberField({ initial: 0 }),
+        totalModifiers: new fields.NumberField({ initial: 0 })
     };
-  }
+}
