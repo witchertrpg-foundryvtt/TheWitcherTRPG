edf4558814101c366572aafaae572aa814ec38ff
Stexinator <hurricane90@gmx.de>
2025-01-28 16:31:49 +0100

    add attack configuration to profession skills

---
+++ module/data/item/templates/combat/skillAttackData.js
@@ -0,0 +1,23 @@
+import damageProperties from './damagePropertiesData.js';
+import defenseOptions from './defenseOptionsData.js';
+
+const fields = foundry.data.fields;
+
+export default function skillAttack() {
+    return {
+        isAttack: new fields.BooleanField({
+            initial: false,
+            label: 'WITCHER.profession.skillPath.skill.skillAttack.isAttack'
+        }),
+        usesWeapon: new fields.BooleanField({
+            initial: false,
+            label: 'WITCHER.profession.skillPath.skill.skillAttack.usesWeapon'
+        }),
+        damageFormulaOverride: new fields.StringField({
+            initial: '',
+            label: 'WITCHER.profession.skillPath.skill.skillAttack.damageFormulaOverrides'
+        }),
+        damageProperties: new fields.SchemaField(damageProperties()),
+        ...defenseOptions()
+    };
+}
