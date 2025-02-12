e618ef632a356137adc069e5ce31ef5c35e5f75e
Stexinator <hurricane90@gmx.de>
2025-02-12 15:15:55 +0100

    move defenseProperties to formGroup

--- module/data/item/templates/combat/defensePropertiesData.js
+++ module/data/item/templates/combat/defensePropertiesData.js
@@ -2,6 +2,6 @@ const fields = foundry.data.fields;
 
 export default function defenseProperties() {
     return {
-        parrying: new fields.BooleanField({ initial: false }),
+        parrying: new fields.BooleanField({ initial: false, label: 'WITCHER.Item.DefenseProperties.parrying' })
     };
 }
