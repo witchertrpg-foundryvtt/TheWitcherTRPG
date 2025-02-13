20e832cf0f976ebb5275f0774b97db99766c35d0
Stexinator <hurricane90@gmx.de>
2025-02-13 08:41:08 +0100

    move consumeProperties to formGroup

--- module/data/item/templates/consumableData.js
+++ module/data/item/templates/consumableData.js
@@ -1,10 +1,10 @@
-import consumeProperties from "./consumePropertiesData.js";
+import consumeProperties from './consumePropertiesData.js';
 
 const fields = foundry.data.fields;
 
 export default function consumable() {
     return {
-        isConsumable: new fields.BooleanField({ initial: false }),
-        consumeProperties: new fields.SchemaField(consumeProperties()),
+        isConsumable: new fields.BooleanField({ initial: false, label: 'WITCHER.Item.isConsumable' }),
+        consumeProperties: new fields.SchemaField(consumeProperties())
     };
 }
