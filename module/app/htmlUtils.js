9431613c0d7493bbd5acf3fa0289f2d17014fcaa
Stexinator <hurricane90@gmx.de>
2025-07-29 13:10:35 +0200

    add currency rewards

--- module/app/htmlUtils.js
+++ module/app/htmlUtils.js
@@ -1,11 +1,25 @@
 export function createLabeledInput(title, type, name) {
-        let label = document.createElement('label');
-        label.style = 'display: flex; gap: 10px; align-items: center';
-        label.innerHTML = title;
+    let label = document.createElement('label');
+    label.style = 'display: flex; gap: 10px; align-items: center';
+    label.innerHTML = title;
 
-        let input = document.createElement('input');
-        input.type = type;
-        input.name = name;
-        label.appendChild(input);
-        return label;
-    }
+    let input = document.createElement('input');
+    input.type = type;
+    input.name = name;
+    label.appendChild(input);
+    return label;
+}
+
+export function createLabeledSelect(title, name, options) {
+    let label = document.createElement('label');
+    label.style = 'display: flex; gap: 10px; align-items: center';
+    label.innerHTML = title;
+
+    let select = foundry.applications.fields.createSelectInput({
+        name: name,
+        ...options
+    });
+
+    label.appendChild(select);
+    return label;
+}
