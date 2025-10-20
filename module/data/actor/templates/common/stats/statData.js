8faf7e0c76163327aefc9049362c45f2aef09fd2
Stexinator <hurricane90@gmx.de>
2025-10-20 09:30:52 +0200

    deprecate stat and skill modifiers

--- module/data/actor/templates/common/stats/statData.js
+++ module/data/actor/templates/common/stats/statData.js
@@ -8,7 +8,6 @@ export default function stat(label) {
         current: new fields.NumberField({ initial: 0}),
         total: new fields.NumberField({ initial: 0}),
         label: new fields.StringField({ initial: label}),
-        isOpened: new fields.BooleanField({ initial: false}),
         modifiers: new fields.ArrayField(new fields.SchemaField(modifier())),
         totalModifiers: new fields.NumberField({ initial: 0 }),
     };
