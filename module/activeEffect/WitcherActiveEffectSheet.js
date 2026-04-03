3ec6d360751277f182a984783b558bd4daa5a272
Stexinator <hurricane90@gmx.de>
2026-04-03 10:22:06 +0200

    move system specific active effect additions to own tab

--- module/activeEffect/WitcherActiveEffectSheet.js
+++ module/activeEffect/WitcherActiveEffectSheet.js
@@ -14,13 +14,21 @@ export class WitcherActiveEffectConfig extends foundry.applications.sheets.Activ
     static PARTS = {
         header: { template: 'templates/sheets/active-effect/header.hbs' },
         tabs: { template: 'templates/generic/tab-navigation.hbs' },
-        details: { template: 'systems/TheWitcherTRPG/templates/sheets/activeEffect/details.hbs', scrollable: [''] },
+        details: { template: 'templates/sheets/active-effect/details.hbs', scrollable: [''] },
         duration: { template: 'templates/sheets/active-effect/duration.hbs' },
-        changes: {
-            template: 'systems/TheWitcherTRPG/templates/sheets/activeEffect/active-effect-changes.hbs',
-            scrollable: ['ol[data-changes]']
+        changes: { template: 'templates/sheets/active-effect/changes.hbs', scrollable: ['ol[data-changes]'] },
+        systemSpecific: {
+            template: 'systems/TheWitcherTRPG/templates/sheets/activeEffect/system-specific.hbs',
+            scrollable: ['']
         },
         footer: { template: 'templates/generic/form-footer.hbs' }
+    };
+
+    static TABS = {
+        sheet: {
+            ...super.TABS.sheet,
+            tabs: [...super.TABS.sheet.tabs, { id: 'systemSpecific' }]
+        }
     };
 
     /** @override */
@@ -28,6 +36,20 @@ export class WitcherActiveEffectConfig extends foundry.applications.sheets.Activ
         const context = await super._prepareContext(options);
         context.systemFields = this.document.system.schema.fields;
         return context;
+    }
+
+    _onRender(context, options) {
+        super._onRender(context, options);
+
+        this.autocomplete();
+
+        const addButton = this.element.querySelector('button.inline-control.icon.fa-regular.fa-square-plus');
+        const wizard = document.createElement('a');
+        wizard.setAttribute('data-action', 'wizard');
+        const icon = document.createElement('i');
+        icon.className = 'fa-solid fa-wand-magic-sparkles';
+        wizard.appendChild(icon);
+        addButton.parentNode.insertBefore(wizard, addButton.nextSibling);
     }
 
     static async wizardAction() {
@@ -68,6 +90,46 @@ export class WitcherActiveEffectConfig extends foundry.applications.sheets.Activ
                 }
             }
         });
+    }
+
+    async autocomplete() {
+        let html = this.element;
+        const effectsSection = html.querySelector("section[data-tab='changes']");
+        const inputFields = effectsSection.querySelectorAll('.key input');
+        const datalist = document.createElement('datalist');
+        const attributeKeyOptions = {};
+
+        datalist.id = 'attribute-key-list';
+        inputFields.forEach(inputField => {
+            inputField.setAttribute('list', 'attribute-key-list');
+        });
+
+        let config;
+        if (this.document.parent.documentName === 'Actor') {
+            config = CONFIG.Actor;
+        }
+
+        if (this.document.parent.documentName === 'Item') {
+            config = CONFIG.Item;
+        }
+
+        for (const datamodel in config.dataModels) {
+            config.dataModels[datamodel].schema.apply(function () {
+                if (!(this instanceof foundry.data.fields.SchemaField)) {
+                    attributeKeyOptions[this.fieldPath] = game.i18n.localize(this.label);
+                }
+            });
+        }
+
+        const sortedKeys = Object.keys(attributeKeyOptions).sort();
+        sortedKeys.forEach(key => {
+            const attributeKeyOption = document.createElement('option');
+            attributeKeyOption.value = key;
+            if (!!attributeKeyOptions[key]) attributeKeyOption.label = attributeKeyOptions[key];
+            datalist.appendChild(attributeKeyOption);
+        });
+
+        effectsSection.append(datalist);
     }
 }
 
